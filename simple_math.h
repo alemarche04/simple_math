59cc393665ef9642b3564b6dc05314f9550dac74
Alessia Marchese <ale09.marchese@gmail.com>
2025-02-27 14:06:16 +0100

    new version of simple_math with max - tested

--- simple_math.h
+++ simple_math.h
@@ -7,6 +7,7 @@
 #ifndef _SIMPLEMATH_  //se questo simbolo non e' mai stato definito
 #define _SIMPLEMATH_  //allora definisci _SIMPLEMATH_
 
-float mean (int a, int b);
+float mean (int a, int b); //restituisce la media dei due parametri
+int max(int a, int b); //restituisce il maggiore tra i due parametri
 
 #endif

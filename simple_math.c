e3503c108908e75a47c0ba3de1cdd92ab7e85737
Alessia Marchese <ale09.marchese@gmail.com>
2025-02-27 13:26:31 +0100

    first version of simple math - no test

---
+++ simple_math.c
@@ -0,0 +1,11 @@
+/**	\file simple_math.h
+		\brief definition of the function
+		\author Alessia Marchese
+*/
+
+#include "simple_math.h"
+
+float mean(int a, int b) // definizione della funzione
+{
+    return(a + b) / 2;
+}
---
+++ simple_math.h
@@ -0,0 +1,12 @@
+/**	\file simple_math.h
+		\brief a toy example of math library
+		\author Alessia Marchese
+*/
+
+ //direttive del preprocessore:
+#ifndef _SIMPLEMATH_  //se questo simbolo non e' mai stato definito
+#define _SIMPLEMATH_  //allora definisci _SIMPLEMATH_
+
+float mean (int a, int b);
+
+#endif
